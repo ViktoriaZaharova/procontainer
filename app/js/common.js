9f209c5cee2c3331022a8c7b76c41df0b48e48a7
Viktoria <zaharova181114@gmail.com>
2022-08-13 19:24:21 +0300

    add 1 section

--- app/js/common.js
+++ app/js/common.js
@@ -4,3 +4,31 @@ $('.home-slider').slick({
     dots: true,
     arrows: false,
 });
+
+$(function () {
+    // hidden list > 5
+    $('.sidebar-box > .list-checkbox').each(function () {
+        if ($(this).find('li').length > 10) {
+            $(this).find('li').slice(10).hide();
+            $(this).parents('.sidebar-box').append('<a href="#" class="load-more-link">Еще <span>5</span></a>');
+        }
+
+    });
+
+    // hidden list > 5
+
+    // show list all
+    $('.load-more-link').on('click', function (e) {
+        e.preventDefault();
+        $(this).prev('.list-checkbox').find('li:hidden').slice(0, 5).slideDown();
+
+        var onBlock = $(this).prev('.list-checkbox').find('li:hidden').length;
+        if(onBlock <= 0) {
+            $(this).hide();
+        }
+    });
+
+    // show list all
+
+
+});
