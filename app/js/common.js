dfa103550579bcdbe3250a94ba5a389423109ef5
Viktoria <zaharova181114@gmail.com>
2022-09-03 13:49:34 +0300

    add page blog+objects

--- app/js/common.js
+++ app/js/common.js
@@ -68,4 +68,15 @@ $('.btn-close-sidebar').on('click', function () {
 });
 
 
+$('.go_to').click(function (e) {
+    e.preventDefault();
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
+});
+
 
